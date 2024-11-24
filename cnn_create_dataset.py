9dcba0bbfefb9368c3de59706ef823e59f90e9e8
thesavarni <pant.savarni@gmail.com>
2024-11-24 19:34:59 +0530

    adding support for 2 hands

--- cnn_create_dataset.py
+++ cnn_create_dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=True,
-    max_num_hands=1,
+    max_num_hands=2,
     min_detection_confidence=0.3
 )
 
